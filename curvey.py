aa6a784882ba2542177f5ad550ad882fa99fe9a8
Elben Shira <elbenshira@gmail.com>
2010-11-12 20:53:10 -0600

    added interpolation, control points generation, and started BSpline class

--- curvey.py
+++ curvey.py
@@ -1,5 +1,38 @@
 # Curvey
 
+class BSpline(object):
+    def __init__(self, points=None, knotvec=None):
+        self.points = points if points else []
+        self.old_points = points if points else []
+        self.knotvec = knotvec if knotvec else []
+
+    def insert(self, knot):
+        self.knotvec.insert(knot)
+        old_vec = self.knotvec.old_vec
+        control_points = self.knotvec.control_points()
+        for i, cp in enumerate(control_points):
+            if cp in self.points:
+                # Control point already exists, so we don't need to recalculate
+                # its x, y.
+                continue
+
+            # New control point. Interpolate between the control points next to
+            # it.
+
+            # TODO we might be at the ends, so i-1 and i+1 might crash. If we're
+            # at the end, then... not sure.
+
+            left = control_points[i-1]
+            right = control_points[i+1]
+            
+            # Push into self.points.
+
+    def render(self, dt=.1):
+        """
+        Run the de Boor algorithm. Return list of x, y coordinates, ready to be
+        rendered.
+        """
+
 class ControlPoint(object):
     def __init__(self, x=0, y=0, knots=None):
         self.x = x
@@ -23,6 +56,35 @@ class ControlPoint(object):
                 return v1 - v2
         return 0    # equal
 
+    @staticmethod
+    def interpolate(left, right, middle, degree=3):
+        """
+        Given left, right, and middle ControlPoints, sets middle's x, y
+        values.
+
+        Returns tuple (a, b, c), which are the differring knots for left,
+        right, and middle respectively.
+
+        Assumes the KnotVectors of each ControlPoints are legit.
+        """
+
+        # TODO this is going to break if there is more than one differring knots
+        # and stuff. Stop and unit test this stuff!!!
+        left.knots.sort()
+        middle.knots.sort()
+        right.knots.sort()
+        for i in range(degree):
+            a = left.knots.at(i)
+            b = right.knots.at(i)
+            c = middle.knots.at(i)
+            if a == b == c:
+                continue
+
+            # The knots differ, so interpolate.
+            middle.x = float((b-c)*left.x + (c-a)*right.x)/(b-a)
+            middle.y = float((b-c)*left.y + (c-a)*right.y)/(b-a)
+            return a, b, c
+
 class KnotVector(object):
     def __init__(self, vec=None, degree=3):
         self.degree = degree
@@ -30,6 +92,25 @@ class KnotVector(object):
 
         # We also save the old vector for insertions.
         self.old_vec = self.vec[:]
+
+    def at(self, i):
+        return self.vec[i]
+
+    def control_points(self, return_old=False):
+        """
+        Return a list of KnotVectors that corresponds to the polar notation for
+        the control points.
+        """
+        vectors = []
+
+        if len(self.vec) < 3:
+            return vectors
+
+        i = 0
+        while i <= (len(self.vec) - 3):
+            vectors.append(KnotVector(self.vec[i:i+3]))
+            i += 1
+        return vectors
 
     def is_valid(self):
         # Returns true if this is valid knot vector.
--- test_curvey.py
+++ test_curvey.py
@@ -1,45 +1,72 @@
-import curvey
+from curvey import *
 import unittest
 
-class TestControlPoitn(unittest.TestCase):
+class TestBSpline(unittest.TestCase):
+    pass
+
+class TestControlPoint(unittest.TestCase):
     def setUp(self):
-        self.cp1 = curvey.ControlPoint()
+        self.cp1 = ControlPoint()
+
     def test_cmp(self):
-        cp1 = curvey.ControlPoint(knots=range(3))
-        cp2 = curvey.ControlPoint(knots=range(3))
+        cp1 = ControlPoint(knots=range(3))
+        cp2 = ControlPoint(knots=range(3))
         self.assertTrue(cp1 == cp2)
 
-        cp1 = curvey.ControlPoint(knots=[0,0,0])
-        cp2 = curvey.ControlPoint(knots=[0,0,1])
+        cp1 = ControlPoint(knots=[0,0,0])
+        cp2 = ControlPoint(knots=[0,0,1])
         self.assertTrue(cp1 < cp2)
 
-        cp1 = curvey.ControlPoint(knots=[0,1,2])
-        cp2 = curvey.ControlPoint(knots=[0,2,3])
+        cp1 = ControlPoint(knots=[0,1,2])
+        cp2 = ControlPoint(knots=[0,2,3])
         self.assertTrue(cp1 < cp2)
         self.assertTrue(cp2 > cp1)
 
-        cp1 = curvey.ControlPoint(knots=[4,4,4])
-        cp2 = curvey.ControlPoint(knots=[3,4,4])
+        cp1 = ControlPoint(knots=[4,4,4])
+        cp2 = ControlPoint(knots=[3,4,4])
         self.assertTrue(cp1 > cp2)
+    
+    def test_interpolate(self):
+        cp1 = ControlPoint(x=1, y=1, knots=[0,0,0])
+        cp2 = ControlPoint(x=6, y=4, knots=[0,0,1])
+        cp3 = ControlPoint(x=0, y=0, knots=[0,0,0.5])
+
+        # a = 0, b = 1, c = 0.5
+        # ((b-a) cp1 + (c-a) cp2) / (c-a)
+        ControlPoint.interpolate(cp1, cp2, cp3)
+        self.assertEqual(cp3.x, 3.5)
+        self.assertEqual(cp3.y, 2.5)
+
+        ############################
+
+        cp1 = ControlPoint(x=1, y=1, knots=[1,1,0])
+        cp2 = ControlPoint(x=6, y=4, knots=[0,4,1])
+        cp3 = ControlPoint(x=0, y=0, knots=[2,1,0])
+
+        # a = 0, b = 1, c = 0.5
+        # ((b-a) cp1 + (c-a) cp2) / (c-a)
+        ControlPoint.interpolate(cp1, cp2, cp3)
+        self.assertEqual(cp3.x, 8.0/3)
+        self.assertEqual(cp3.y, 2.0)
 
 class TestKnotVector(unittest.TestCase):
     def setUp(self):
-        self.kv1 = curvey.KnotVector()
-        self.kv2 = curvey.KnotVector(range(5))
-        self.kv3 = curvey.KnotVector([3,3,3,4,5,6,6,6])
+        self.kv1 = KnotVector()
+        self.kv2 = KnotVector(range(5))
+        self.kv3 = KnotVector([3,3,3,4,5,6,6,6])
 
     def test_is_valid(self):
         self.assertTrue(self.kv1.is_valid())
         self.assertTrue(self.kv2.is_valid())
         self.assertTrue(self.kv3.is_valid())
 
-        kv = curvey.KnotVector([1,1,1,0])
+        kv = KnotVector([1,1,1,0])
         self.assertFalse(kv.is_valid())
 
-        kv = curvey.KnotVector([0,1,1,0])
+        kv = KnotVector([0,1,1,0])
         self.assertFalse(kv.is_valid())
 
-        kv = curvey.KnotVector([4,3,2,1])
+        kv = KnotVector([4,3,2,1])
         self.assertFalse(kv.is_valid())
 
     def test_insert(self):
@@ -62,9 +89,9 @@ class TestKnotVector(unittest.TestCase):
         self.assertEqual(self.kv1.vec, [0,1,1,1,5,10])
 
     def test_equal(self):
-        kv1 = curvey.KnotVector()
-        kv2 = curvey.KnotVector(range(5))
-        kv3 = curvey.KnotVector([3,3,3,4,5,6,6,6])
+        kv1 = KnotVector()
+        kv2 = KnotVector(range(5))
+        kv3 = KnotVector([3,3,3,4,5,6,6,6])
         self.assertEqual(self.kv1, kv1)
         self.assertEqual(self.kv2, kv2)
         self.assertEqual(self.kv3, kv3)
@@ -84,6 +111,27 @@ class TestKnotVector(unittest.TestCase):
         self.assertEqual(kv3copy, self.kv3)
         kv3copy.insert(100)
         self.assertNotEqual(kv3copy, self.kv3)
+
+    def test_control_points(self):
+        self.assertEqual(self.kv1.control_points(), [])
+        
+        kv2pts = [KnotVector(range(3)),
+                  KnotVector(range(1,4)),
+                  KnotVector(range(2,5)),
+                  KnotVector(range(3,6))]
+        pts = self.kv2.control_points();
+        for p1, p2 in zip(pts, kv2pts):
+            self.assertEqual(p1, p2)
+        
+        kv3pts = [KnotVector([3,3,3]),
+                  KnotVector([3,3,4]),
+                  KnotVector([3,4,5]),
+                  KnotVector([4,5,6]),
+                  KnotVector([5,6,6]),
+                  KnotVector([6,6,6])]
+        pts = self.kv3.control_points();
+        for p1, p2 in zip(pts, kv3pts):
+            self.assertEqual(p1, p2)
 
 
 if __name__ == '__main__':
