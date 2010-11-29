d6a199f7628ef91fce05698a414b802d051a4b8f
Elben Shira <elbenshira@gmail.com>
2010-11-29 13:27:12 -0600

    removed plane_w and plane_h variables; made UI smaller

--- uitk.py
+++ uitk.py
@@ -17,7 +17,7 @@ dt=0.2
     def __init__(self, control_points=None, draw_points=None, degree=3,
             background_color="#cccccc", point_color="#ff0000",
             line_color="#009900",
-            canvas_w=640, canvas_h=320, plane_w=640, plane_h=320):
+            canvas_w=640, canvas_h=320):
         self.degree = degree
         self.dt = None
         self.control_points = control_points
@@ -28,18 +28,13 @@ dt=0.2
         self.line_color = line_color
         self.canvas_w = canvas_w
         self.canvas_h = canvas_h
-        self.plane_w = plane_w if plane_w else canvas_w
-        self.plane_h = plane_h if plane_h else canvas_h
-
-        self.dw = (self.canvas_w - self.plane_w)/2
-        self.dh = (self.canvas_h - self.plane_h)/2
 
         self.master = Tk()
         self.master.title("Curvey")
 
         self.frame = Frame(self.master)
 
-        self.editbox = Text(self.frame, bg='#cccccc', borderwidth=4, width=30,
+        self.editbox = Text(self.frame, bg='#cccccc', borderwidth=4, width=20,
                 height=30)
         self.editbox.insert('0.0', UI.default_control_points)
         
@@ -64,7 +59,7 @@ dt=0.2
         self.editbox.grid(row=2, column=0, columnspan=2)
         self.canvas.grid(row=2, column=3, columnspan=2)
 
-        self.draw_labels_checkbox.grid(row=0, column=0)
+        self.draw_labels_checkbox.grid(row=0, column=0, columnspan=2)
         self.renderbutton.grid(row=1, column=0)
         self.clearbutton.grid(row=1, column=1)
 
@@ -95,19 +90,23 @@ dt=0.2
             bspline.insert_control_point(p)
         bspline.replace_knot_vector(knotvecs[0])
 
-        # Scale and translate points for drawing.
-        control_points, self.control_point_polars, points = bspline.render()
+        if bspline.is_valid():
+            # Scale and translate points for drawing.
+            control_points, self.control_point_polars, points = bspline.render()
 
-        self.control_points = transform_for_canvas(control_points,
-                self.plane_w, self.plane_h, 32, 32)
-        self.draw_points = transform_for_canvas(points, self.plane_w,
-                self.plane_h, 32, 32)
+            self.control_points = transform_for_canvas(control_points,
+                    self.canvas_w, self.canvas_h, 32, 32)
+            self.draw_points = transform_for_canvas(points, self.canvas_w,
+                    self.canvas_h, 32, 32)
 
-        self.control_points = mirror_y(self.control_points, about_y=160)
-        self.draw_points = mirror_y(self.draw_points, about_y=160)
+            self.control_points = mirror_y(self.control_points, about_y=160)
+            self.draw_points = mirror_y(self.draw_points, about_y=160)
 
-        # Draw.
-        self.draw()
+            # Draw.
+            self.draw()
+        else:
+            error_msg = "Invalid curve specified.\nMake sure you have the right number of points for the degree and knot vector specified."
+            self.canvas.create_text(self.canvas_w/2, self.canvas_h/2-100, text=error_msg)
 
     def draw_labels(self):
         magic = -10
@@ -117,21 +116,21 @@ dt=0.2
             
             polar = str(self.control_point_polars[i])
             label = "%d %s" % (i, polar)
-            self.canvas.create_text(x+self.dw, y+self.dh+magic, text=label)
+            self.canvas.create_text(x, y+magic, text=label)
 
     def draw(self):
         # Draw control points
         for i, cp in enumerate(self.control_points):
             x, y = tuple(cp)
             radius = 4 # pixels
-            self.canvas.create_oval(x-radius+self.dw, y-radius+self.dh,
-                    x+radius+self.dw, y+radius+self.dh, fill="#ff0000")
+            self.canvas.create_oval(x-radius, y-radius,
+                    x+radius, y+radius, fill="#ff0000")
 
         # Draw points
         for i in range(len(self.draw_points)-1):
             x1, y1 = tuple(self.draw_points[i])
             x2, y2 = tuple(self.draw_points[i+1])
-            self.canvas.create_line(x1+self.dw, y1+self.dh, x2+self.dw, y2+self.dh, fill="blue")
+            self.canvas.create_line(x1, y1, x2, y2, fill="blue")
 
         if self.drawing_labels:
             self.draw_labels()
